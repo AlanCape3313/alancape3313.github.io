fb001b7a9a949e88dd109550fad48ea8f60479ca
Alan Capellino <alancapellino@honeyfrost.net>
2025-09-19 11:29:17 -0300

    a

--- script.js
+++ script.js
@@ -18,15 +18,3 @@ skinViewer.background = 0x5a76f3;
 // Cámara y zoom
 skinViewer.fov = 70;
 skinViewer.zoom = 0.5;
-
-const canvas = document.getElementById(`skin_container`);
-
-canvas.addEventListener(`mousemove`, (event) => {
-    const rect = canvas.getBoundingClientRect();
-    
-    const x = ((event.clientX - rect.left) / rect.width - 0.5) * 2;  
-    const y = ((event.clientY - rect.top) / rect.height - 0.5) * 2;
-
-    skinViewer.player.rotation.y = x * Math.PI;        // de -180° a 180°
-    skinViewer.player.rotation.x = -y * Math.PI * 0.25; // un cuarto de rotación vertical
-})
