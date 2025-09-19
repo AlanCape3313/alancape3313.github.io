69e61223804066c9872f6c72a475da565c53c228
AlanCape3313 <alanezequielcapellino2003@gmail.com>
2025-09-19 18:20:02 -0300

    awds

--- behaviors/main.js
+++ behaviors/main.js
@@ -1,3 +1,7 @@
+import * as THREE from "three";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
